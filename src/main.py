eb3dd92f1d6cdf893857327111058bf0ee00b3f1
appcreatorguy <appcreatorguy@users.noreply.github.com>
2021-06-05 20:44:05 +0000

    fixup: Format Python code with Black

--- src/main.py
+++ src/main.py
@@ -11,19 +11,24 @@ __version__ = "0.0.1"
 __license__ = "GPLv3"
 
 creatures = [] * 12
-food = [] * 60 #* length of 60, int food in each
-class Creature():
+food = [] * 60  # * length of 60, int food in each
+
+
+class Creature:
     def __init__(self):
         super(Creature, self).__init__()
         self.hunger = 0
-        self.food_position = None #* Index of food list
+        self.food_position = None  # * Index of food list
         self.alive = True
-    def pick_food(self, food_list = food):
+
+    def pick_food(self, food_list=food):
         self.food_position = random.randint(1, len(food_list))
-    def eat_food(self, food_list = food):
+
+    def eat_food(self, food_list=food):
         self.hunger = food_list[self.food_position]
         food_list[self.food_position] = 0
-    def sleep(self, creatures_list = creatures):
+
+    def sleep(self, creatures_list=creatures):
         if self.hunger == 0:
             # Die
             self.alive = False
@@ -33,28 +38,29 @@ class Creature():
         elif self.hunger == 2:
             # Reproduce
             creatures_list.append(Creature())
-                        
-            
-        
-def init_creatures(creatures_list = creatures):
-    creatures_list = [Creature()] * 12 # Creates 12 empty creatures
 
-def init_food(food_list = food):
-    food_list = [2] * 60 # Resets Food
 
-def pick_all_food(creatures_list = creatures):
+def init_creatures(creatures_list=creatures):
+    creatures_list = [Creature()] * 12  # Creates 12 empty creatures
+
+
+def init_food(food_list=food):
+    food_list = [2] * 60  # Resets Food
+
+
+def pick_all_food(creatures_list=creatures):
     for creature in creatures_list:
         if creature.alive:
             creature.pick_food()
     init_food()
 
-def eat_all_food(food_list = food, creatures_list = creatures):
+
+def eat_all_food(food_list=food, creatures_list=creatures):
     # TODO: Add double detection
     for creature in creatures_list:
         if creature.alive:
             creature.eat_food()
 
-    
 
 def main():
     print("hello world")
