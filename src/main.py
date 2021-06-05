090b746e2e62ae571da15f5edda636ee13e15c05
appcreatorguy <manmenonsense@gmail.com>
2021-06-05 19:41:24 +0100

    added autoformatting and added python boilerplate

---
+++ src/main.py
@@ -0,0 +1,18 @@
+"""
+Evolution Simulator
+
+A simple Predator-Prey Simulator
+"""
+
+__author__ = "Manas Mengle"
+__version__ = "0.0.1"
+__license__ = "MIT"
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
+    main()
