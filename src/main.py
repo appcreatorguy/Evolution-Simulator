770e8236e4775966480e3af7e3e079c747502a8d
appcreatorguy <manmenonsense@gmail.com>
2021-06-06 11:28:55 +0100

    Added Main game loop

--- src/main.py
+++ src/main.py
@@ -10,26 +10,31 @@ __license__ = "GPLv3"
 
 
 import random
+import time
 
-creatures = [] * 12
-food = [] * 60  # * length of 60, int food in each
+creatures = []
+new_creatures = [] # * Creatures to be added after current ones have gone to sleep
+foods = []  # * length of 60, int food in each
 
 
 class Creature:
     def __init__(self):
         super(Creature, self).__init__()
         self.hunger = 0
-        self.food_position = None  # * Index of food list
+        self.food_index = None  # * Index of food list
         self.alive = True
+    
+    def reset_hunger(self):
+        self.hunger = 0
 
-    def pick_food(self, food_list=food):
-        self.food_position = random.randint(1, len(food_list))
+    def pick_food(self):
+        self.food_index = random.randint(1, len(foods)-1) # len - 1 due to last index of list being len - 1, not len
 
-    def eat_food(self, food_list=food):
-        self.hunger = food_list[self.food_position]
-        food_list[self.food_position] = 0
+    def eat_food(self):
+        self.hunger = foods[self.food_index]
+        return self.hunger
 
-    def sleep(self, creatures_list=creatures):
+    def sleep(self):
         if self.hunger == 0:
             # Die
             self.alive = False
@@ -38,39 +43,88 @@ class Creature:
             pass
         elif self.hunger == 2:
             # Reproduce
-            creatures_list.append(Creature())
+            new_creatures.append(Creature())
 
 
-def reset_creatures(creatures_list=creatures):
-    creatures_list = [Creature()] * 12  # Creates 12 empty creatures
+def reset_creatures():
+    global creatures
+    for i in range(12):
+        creatures.append(Creature())  # Creates 12 empty creatures
+        
 
 
-def reset_food(food_list=food):
-    food_list = [2] * 60  # Resets Food
+def reset_food():
+    global foods
+    foods = [2] * 60  # Resets Food
+
+def reset_hunger():
+    for creature in creatures:
+        if creature.alive:
+            creature.reset_hunger()
 
 
-def pick_all_food(creatures_list=creatures):
-    for creature in creatures_list:
+def pick_all_food():
+    for creature in creatures:
         if creature.alive:
             creature.pick_food()
-    reset_food()
 
 
-def eat_all_food(food_list=food, creatures_list=creatures):
+def eat_all_food():
     # TODO: Add double detection
-    for creature in creatures_list:
+    for creature in creatures:
         if creature.alive:
             creature.eat_food()
+            foods[creature.food_index] -= creature.hunger
 
 
-def sleep_all(creatures_list=creatures):
-    for creature in creatures_list:
+def sleep_all():
+    for creature in creatures:
         if creature.alive:
             creature.sleep()
+    creatures.extend(new_creatures)
+    new_creatures.clear()
 
+
+
+def print_food():
+    print('\n\n')
+    print('Food: ', end='')
+    for food in foods:
+        print(food, end='')
+def print_creatures():
+    print('\n\n')
+    print("Number of creatures:",len(creatures))
+    print("\nCreature | Hunger")
+    for creature in creatures:
+        if creature.alive:
+            print(creatures.index(creature)+1,creature.hunger)
+def main_game_loop(days):
+    reset_creatures()
+    for day in range(days):
+        print("\n\n DAY",day+1,"\n\n")
+        reset_food()
+        print_food()
+        time.sleep(1)
+        print_creatures()
+        time.sleep(2)
+        pick_all_food()
+        print("Food Picked")
+        eat_all_food()
+        time.sleep(2)
+        print("Food Eaten")
+        print_creatures()
+        time.sleep(2)
+        print_food()
+        time.sleep(2)
+        print("Creatures Reproduced, survived or died")
+        sleep_all()
+        reset_hunger()
+        print_creatures()
+        time.sleep(5)
 
 def main():
     print("hello world")
+    main_game_loop(2)
 
 
 if __name__ == "__main__":
