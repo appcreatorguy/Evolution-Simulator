ff1183c8501f7eeca4f72e31aa96153f43534e2a
appcreatorguy <manmenonsense@gmail.com>
2021-06-25 18:31:27 +0100

    Seperate Simulation to a new module Fixes #22

--- src/evolution-simulator/main.py
+++ src/evolution-simulator/main.py
@@ -8,162 +8,11 @@ __author__ = "Manas Mengle"
 __version__ = "0.2.0"
 __license__ = "GPLv3"
 
-
-import random
+import simulator
 import time
-from enum import Enum, auto
 import argparse
-import matplotlib.pyplot as plt
-import numpy as np
-
-creatures = []
-new_creatures = []  # * Creatures to be added after current ones have gone to sleep
-foods = []  # * Food 'map'. Each item holds int food amount
-score_table = {} # * Should be written to in the format 'ROUND INDEX' : CREATURE COUNT
 
 
-class State(Enum):
-    SURVIVE = auto()
-    REPRODUCE = auto()
-    DIE = auto()
-
-
-class Creature:
-    def __init__(self):
-        super(Creature, self).__init__()
-        self.hunger = 0
-        self.food_index = None  # * Index of food list
-        self.alive = True
-
-    def reset_hunger(self):
-        self.hunger = 0
-
-    def pick_food(self):
-        self.food_index = random.randint(
-            1, len(foods) - 1
-        )  # len - 1 due to last index of list being len - 1, not len
-
-    def eat_food(self):
-        self.hunger = foods[self.food_index].nutrition
-
-    def sleep(self):
-        if self.hunger == 0:
-            # Die
-            self.alive = False
-            return State.DIE
-        elif self.hunger == 1:
-            # Survive
-            return State.SURVIVE
-        elif self.hunger == 2:
-            # Reproduce
-            new_creatures.append(Creature())
-            return State.REPRODUCE
-class Food:
-    def __init__(self, food_amount):
-        # super(Food, self).__init__(food_amount) # dunno if this is needed?
-        self.nutrition = food_amount
-
-def consumption_handler(creature, food):
-    creature.eat_food()
-    food.nutrition -= creature.hunger
-
-def reset_creatures(amount=12):
-    global creatures
-    for i in range(amount):
-        creatures.append(Creature())  # Creates 12 empty creatures
-
-
-def reset_food(amount=60):
-    global foods
-    for i in range(amount):
-        foods.append(Food(2)) #Reset Food
-
-def reset_hunger():
-    for creature in creatures:
-        if creature.alive:
-            creature.reset_hunger()
-
-
-def pick_all_food():
-    for creature in creatures:
-        if creature.alive:
-            creature.pick_food()
-
-
-def eat_all_food():
-    # TODO: Add double detection
-    for creature in creatures:
-        if creature.alive:
-            consumption_handler(creature,foods[creature.food_index])
-
-
-def sleep_all():
-    for creature in creatures:
-        if creature.alive:
-            state = creature.sleep()
-            if state == State.SURVIVE:
-                print("Creature", creatures.index(creature) + 1, "survived.")
-            elif state == State.REPRODUCE:
-                print("Creature", creatures.index(creature) + 1, "reproduced.")
-            elif state == State.DIE:
-                print("Creature", creatures.index(creature) + 1, "died.")
-
-    creatures.extend(new_creatures)
-    new_creatures.clear()
-
-
-def print_food():
-    print("\n\n")
-    print("Food: ", end="")
-    for food in foods:
-        print(food.nutrition, end="")
-        time.sleep(0.1)
-
-
-def print_creatures():
-    print("\n\n")
-    alive_creatures = [creature for creature in creatures if creature.alive]
-    print("Number of creatures:", len(alive_creatures))
-    print("\nCreature | Hunger")
-    for creature in creatures:
-        if creature.alive:
-            print(creatures.index(creature) + 1, creature.hunger)
-            time.sleep(0.2)
-
-
-def main_game_loop(days, creature_amount, food_amount, *args):
-    reset_creatures(creature_amount)
-    for day in range(days):
-        skip = True if args[0] > 0 else False
-        print("\n\n DAY", day + 1, "\n\n")
-        reset_food(food_amount)
-        print_food()
-        None if skip else time.sleep(2)
-        print_creatures()
-        None if skip else time.sleep(5)
-        pick_all_food()
-        print("Food Picked")
-        eat_all_food()
-        None if skip else time.sleep(4)
-        print("Food Eaten")
-        print_creatures()
-        None if skip else time.sleep(5)
-        print_food()
-        None if skip else time.sleep(4)
-        print("\nCreatures Reproduced, survived or died")
-        sleep_all()
-        None if skip else time.sleep(4)
-        reset_hunger()
-        None if args[0] > 1 else time.sleep(10)
-        score_writer(len(creatures), day)
-    print('FINAL SCORE')
-    score_keys = list(score_table.keys())
-    score_vals = list(score_table.values())
-    for score in score_table.values():
-        print(score_keys[score_vals.index(score)] + 1, ":", score) # Print score fancily
-    # TODO: Add Matplotlib
-def score_writer(score, round):
-    score_table[round] = score
 def main(args):
     """Main entry point of the app"""
     print("EVOLUTION SIMULATOR")
@@ -182,12 +31,12 @@ def main(args):
     print("The cycle then repeats for the following days.")
     None if args.pause > 1 else time.sleep(10)
 
-    print("Running Simulation with:")
+    print("\n\n","Running Simulation with:")
     print(args.days, "days.")
     print(args.start_creatures, "starting creatures.")
     print(args.food_amount, "available food locations.")
 
-    main_game_loop(
+    simulator.main_game_loop(
         int(args.days), int(args.start_creatures), int(args.food_amount), args.pause
     )  # ? Reducing food amount?
 
